47c83e7a03b725380c35e4af8f6dd470c0296b6e
Naios <naios-dev@live.de>
2011-10-25 21:30:28 +0200

    Spawn Points added
    Signed-off-by: Naios <naios-dev@live.de>

---
+++ Misc/Script/Spawn Points.sql
@@ -0,0 +1,14 @@
+
+--	------------------------------------ --
+--			nIneTeen Patch				 --
+--				by Naios    			 --
+--	https://github.com/Naios/nineteen	 --		
+--	------------------------------------ --
+
+-- Creature Spawn Points
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (209422, 45008, 1, 1, 1, 0, 0, -11387.8, -4756.01, 6.17918, 0.374621, 300, 0, 0, 404, 2280, 0, 0, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (209427, 45007, 1, 1, 1, 0, 1, -11461.5, -4762.9, 74.8268, 5.62777, 300, 0, 0, 449, 0, 0, 0, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (209425, 45002, 1, 1, 1, 0, 1, -11389.7, -4703.05, 7.19358, 5.70238, 300, 0, 0, 449, 0, 0, 0, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (209424, 45001, 1, 1, 1, 0, 0, -11389, -4752.08, 6.34305, 0.458282, 300, 0, 0, 404, 2280, 0, 0, 0, 0);
+INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES (209428, 45000, 1, 1, 1, 0, 1, -11520.6, -4723.93, 2.49169, 5.76522, 300, 0, 0, 449, 0, 0, 0, 0, 0);
