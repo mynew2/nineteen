2611466a46e9d0964121de8f47b3edc1b212bb83
Naios <naios-dev@live.de>
2011-10-28 18:54:52 +0200

    Dungeon Twilight Halls begun...
    Signed-off-by: Naios <naios-dev@live.de>

--- Misc/Script/Teleport_Locations.sql
+++ Misc/Script/Teleport_Locations.sql
@@ -15,3 +15,8 @@ INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orient
 DELETE FROM game_tele WHERE name = 'r1';
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1425, 63.8269, 40.226, -4.76295, 1.62575, 565, 'r1');
 
+-- Dungeon Twilight Halls
+DELETE FROM game_tele WHERE name = 'd1';
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1426, -11371.1, -4736.76, 5.01199, 3.47224, 1, 'p1');
+
+
---
+++ Misc/Script/Twilight_Halls.sql
@@ -0,0 +1,32 @@
+
+--	------------------------------------ --
+--			nIneTeen Patch				 --
+--				by Naios    			 --
+--	https://github.com/Naios/nineteen	 --		
+--	------------------------------------ --
+
+-- Twilight Hall
+
+--	Deletion...
+--		Bogstroks
+DELETE FROM `creature` WHERE `id` = '17816';
+DELETE FROM `creature` WHERE `id` = '17817';
+--		Wastewall Worker
+DELETE FROM `creature` WHERE `id` = '17964';
+--		Coilfang Slavehandler
+DELETE FROM `creature` WHERE `id` = '17959';
+--		Wastewall Captive
+DELETE FROM `creature` WHERE `id` = '18206';
+--		Wastewalker Slave
+DELETE FROM `creature` WHERE `id` = '17963';
+--		Ciolfang Champion
+DELETE FROM `creature` WHERE `id` = '17957';
+--		?
+DELETE FROM `creature` WHERE `id` = '18206';
+--		?
+DELETE FROM `creature` WHERE `id` = '18206';
+--		?
+DELETE FROM `creature` WHERE `id` = '18206';
+--		?
+DELETE FROM `creature` WHERE `id` = '18206';
+
